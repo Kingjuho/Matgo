2c62bdd14dcecad6ca76a4ddd71e686a172c717e
Jhlee <ljh990627@gmail.com>
2026-02-22 09:09:39 +0900

    Player 추상 클래스 추가
    이 클래스를 상속시킨 Human, Computer 클래스 제작 예정

---
+++ Assets/_Scripts/Players/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Player : MonoBehaviour
+{
+    [Header("기본 정보")]
+    public string playerName;
+    public long money = 500000;
+
+    [Header("화투 목록")]
+    public List<Card> handCards = new List<Card>();     // 들고 있는 패
+    public List<Card> capturedCards = new List<Card>(); // 먹은 패
+
+    [Header("게임 상태")]
+    public int currentScore = 0;    // 현재 점수
+    public int multiplier = 1;      // 배당
+    public int goCount = 0;         // 고 횟수
+
+    /** 카드 획득 **/
+    public virtual void CaptureCard(Card card)
+    {
+        capturedCards.Add(card);
+    }
+
+    /** 배당 증가 **/
+    public void addMultiplier()
+    {
+        // 변칙적인 배당 증가는 없으므로 2배로 고정
+        multiplier *= 2;
+        Debug.Log($"[{playerName}] 배당이 {multiplier}배로 증가");
+    }
+
+    /** 패 지우기 (턴 시작 시 등) **/
+    public void RemoveHandCard(Card card)
+    {
+        if (handCards.Contains(card))
+            handCards.Remove(card);
+    }
+}
